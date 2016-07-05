ef2042c18b25acad6b535074c8e7724a85cf1d19
WeicyLucy <ying@caece.net>
2016-07-05 15:02:36 +0800

    second rivision

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace _20160705ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hi");
+            Console.WriteLine("second revision");
         }
     }
 }
